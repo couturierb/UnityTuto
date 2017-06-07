11d52e8032ae63e18d78595bafe26afc8d96a495
Boris Couturier <couturier.boris@gmail.com>
2017-06-07 23:12:10 +0200

    (Tuto) Tir enemies

---
+++ Assets/scripts/EnemyScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour
+{
+	private WeaponScript[] weapons;
+
+	void Awake()
+	{
+		// Récupération de toutes les armes de l'ennemi
+		weapons = GetComponentsInChildren<WeaponScript>();
+	}
+
+	void Update()
+	{
+		foreach (WeaponScript weapon in weapons)
+		{
+			// On fait tirer toutes les armes automatiquement
+			if (weapon != null && weapon.CanAttack)
+			{
+				weapon.Attack(true);
+			}
+		}
+	}
+}
+
