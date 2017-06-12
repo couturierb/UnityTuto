6b73a4ed7f5d9cee58f3b6e8a2c968012d62a885
Boris Couturier <couturier.boris@gmail.com>
2017-06-13 00:23:52 +0200

    (Tuto) Scrolling fin

--- Assets/scripts/EnemyScript.cs
+++ Assets/scripts/EnemyScript.cs
@@ -3,23 +3,78 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour
 {
+	private bool hasSpawn;
+	private MoveScript moveScript;
 	private WeaponScript[] weapons;
+	private Collider2D collider2D;
 
 	void Awake()
 	{
 		// Récupération de toutes les armes de l'ennemi
 		weapons = GetComponentsInChildren<WeaponScript>();
+
+		// Récupération du script de mouvement lié
+		moveScript = GetComponent<MoveScript>();
+
+		collider2D = GetComponent<Collider2D>();
+	}
+
+	void Start()
+	{
+		hasSpawn = false;
+
+		// On désactive tout
+		// -- collider
+		collider2D.enabled = false;
+		// -- Mouvement
+		moveScript.enabled = false;
+		Debug.Log (moveScript);
+		// -- Tir
+		foreach (WeaponScript weapon in weapons)
+		{
+			weapon.enabled = false;
+		}
 	}
 
 	void Update()
 	{
+		if (hasSpawn == false) {
+			if (GetComponent<Renderer>().IsVisibleFrom (Camera.main)) {
+				Spawn ();
+			}
+		} else {
+			foreach (WeaponScript weapon in weapons)
+			{
+				// On fait tirer toutes les armes automatiquement
+				if (weapon != null && weapon.enabled && weapon.CanAttack)
+				{
+					weapon.Attack(true);
+				}
+			}
+	
+
+			// 4 - L'ennemi n'a pas été détruit, il faut faire le ménage
+			if (GetComponent<Renderer>().IsVisibleFrom(Camera.main) == false)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	// 3 - Activation
+	private void Spawn()
+	{
+		hasSpawn = true;
+
+		// On active tout
+		// -- Collider
+		collider2D.enabled = true;
+		// -- Mouvement
+		moveScript.enabled = true;
+		// -- Tir
 		foreach (WeaponScript weapon in weapons)
 		{
-			// On fait tirer toutes les armes automatiquement
-			if (weapon != null && weapon.CanAttack)
-			{
-				weapon.Attack(true);
-			}
+			weapon.enabled = true;
 		}
 	}
 }
--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -40,6 +40,30 @@ public class PlayerScript : MonoBehaviour
 			}
 		}
 
+		var dist = (transform.position - Camera.main.transform.position).z;
+
+		var leftBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+		).x;
+
+		var rightBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(1, 0, dist)
+		).x;
+
+		var topBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+		).y;
+
+		var botBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 1, dist)
+		).y;
+
+		transform.position = new Vector3 (
+			Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+			Mathf.Clamp(transform.position.y, topBorder, botBorder),
+			transform.position.z
+		);
+
 	}
 
 	void FixedUpdate()
